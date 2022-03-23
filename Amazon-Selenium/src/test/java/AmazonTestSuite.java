0feca77e3eef789defa095ab878d7d95e9f93f41
Michael Kang <eerielocus@gmail.com>
2022-03-23 11:59:55 -0500

    Small update to testSuite

--- Amazon-Selenium/src/test/java/AmazonTestSuite.java
+++ Amazon-Selenium/src/test/java/AmazonTestSuite.java
@@ -1,4 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -9,15 +11,24 @@ import org.testng.annotations.Test;
 
 public class AmazonTestSuite {
     private AmazonHomePage amazonHomePage;
+    private WebDriver driver;
+    private String book = "The Cucumber Book: Behaviour-Driven Development for Testers and Developers";
 
     @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        amazonHomePage = new AmazonHomePage(new ChromeDriver());
+        driver = new ChromeDriver();
     }
 
     @Test
     public void search() {
+        amazonHomePage = new AmazonHomePage(driver);
         amazonHomePage.useSearchBox("Cucumber Testing");
+        Assert.assertTrue(driver.getPageSource().contains(book));
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
     }
 }
