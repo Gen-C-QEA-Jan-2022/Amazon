030e7c87225727329e9335ac8c337c1c9475abd4
Michael Kang <eerielocus@gmail.com>
2022-03-23 14:23:13 -0500

    Update testSuite to use Cart page.

--- Amazon-Selenium/src/test/java/AmazonTestSuite.java
+++ Amazon-Selenium/src/test/java/AmazonTestSuite.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class AmazonTestSuite {
     private AmazonHomePage amazonHomePage;
+    private AmazonCartPage amazonCartPage;
     private WebDriver driver;
     private String book = "The Cucumber Book: Behaviour-Driven Development for Testers and Developers";
 
@@ -18,6 +19,7 @@ public class AmazonTestSuite {
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     @Test
@@ -25,6 +27,20 @@ public class AmazonTestSuite {
         amazonHomePage = new AmazonHomePage(driver);
         amazonHomePage.useSearchBox("Cucumber Testing");
         Assert.assertTrue(driver.getPageSource().contains(book));
+    }
+
+    @Test(dependsOnMethods = "search")
+    public void cart() {
+        // Temporary usage to get to cart window.
+        driver.findElement(By.cssSelector("img[alt='" + book + "']")).click();
+        driver.findElement(By.id("add-to-cart-button")).click();
+        driver.findElement(By.id("nav-cart")).click();
+        
+        amazonCartPage = new AmazonCartPage(driver);
+        Assert.assertEquals(amazonCartPage.getSubtotal().getText(), "$32.62");
+
+        amazonCartPage.getDelete().click();
+        Assert.assertEquals(amazonCartPage.getEmptyCart().getText(), "Your Amazon Cart is empty.");
     }
 
     @AfterTest
